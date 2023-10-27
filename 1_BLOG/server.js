84d600854ceb9940bbb30b1cfbb311103e4807f6
RAJAN KUMAR <73783957+RK-41@users.noreply.github.com>
2023-10-28 00:34:24 +0530

    Minor tweaks

--- 1_BLOG/server.js
+++ 1_BLOG/server.js
@@ -30,26 +30,15 @@ const articlesInfo = {
 //    Function of express: It parses incoming JSON payload
 app.use(express.json({extended: false}));
 
-// Routing the comments
+// Routing the Comments
 app.post('/api/articles/:name/add-comments', (req, res) => {
    const {username, text} = req.body;
    const articleName = req.params.name;
-   // articlesInfo[articleName].comments.push({username, text});
-   articlesInfo[articleName].comments.push(req.body);
+   articlesInfo[articleName].comments.push({username, text});
+   // articlesInfo[articleName].comments.push(req.body);
 
    res.status(200).send(articlesInfo[articleName]);
 })
-
-// Test route
-app.get('/', (req, res) => (
-   res.send('Hello world')
-))
-app.post('/', (req, res) => (
-   res.send(`Hey ${req.body.name}!`)
-))
-app.get('/hey/:name', (req, res) => (
-   res.send(`Hey, ${req.params.name}!`)
-))
 
 // Starting the server
 app.listen(port, () => console.log(`Server running at port ${port}`))
