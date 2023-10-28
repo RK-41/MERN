d1964976442628464b9696cf36b3c0bcc3676914
RAJAN KUMAR <73783957+RK-41@users.noreply.github.com>
2023-10-28 22:02:32 +0530

    Day 3 Update

--- 1_BLOG/client/src/pages/Article.js
+++ 1_BLOG/client/src/pages/Article.js
@@ -1,14 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import articleContent from './article-content';
 import Articles from '../Components/Articles';
 import NotFound from './NotFound';
+import CommentsList from '../Components/CommentsList';
+import AddCommentForm from '../Components/AddCommentForm';
 
 function Article() {
    const { name } = useParams();
    const article = articleContent.find((article) => article.name === name);
 
-   if(!article) return <NotFound />
+   // Using 'useState()' and 'useEffect()' to Update the Article's Info
+   const [articleInfo, setArticleInfo] = useState({ comments: [] });
+
+   useEffect(() => {
+      const fetchData = async () => {
+         // Fetching Article Info
+         const result = await fetch(`/api/articles/${name}`);
+
+         const body = await result.json();
+
+         console.log(body)
+
+         // Updating the Article Info
+         setArticleInfo(body);
+      };
+
+      fetchData();
+   }, [name]);
+   // The purpose of passing '[name]' as an argument is to make sure that the 'useEffect()' function only executes when there's change in the URL
+
+
+   if (!article) return <NotFound />
 
    const otherArticles = articleContent.filter(article => article.name !== name);
 
@@ -23,6 +46,12 @@ function Article() {
                {para}
             </p>
          ))}
+
+         {/* Comments' List Compnent (28.10.)*/}
+         <CommentsList comments={articleInfo.comments} />
+
+         {/* Add-Comment Form */}
+         <AddCommentForm articleName={name} setArticleInfo={setArticleInfo} />
 
          <h1 className='sm:text-2xl text-xl font-bold my-4 text-gray-900'>
             Other articles
--- 1_BLOG/server.js
+++ 1_BLOG/server.js
@@ -2,42 +2,101 @@
 27.10.
 
    Entry Point of the App
+
+   'localhost' and '127.0.0.1' are not entirely same. Use them at respective places.
  */
 
-// Importing Express framework
+// Importing Modules
 const express = require('express');
+const { MongoClient } = require('mongodb');
 
 // Creating an Express app
 const app = express();
 
 // Port
-const port = process.env.PORT || 8000
-
-// For the Comments
-const articlesInfo = {
-   "learn-react": {
-      comments: []
-   },
-   "learn-node": {
-      comments: []
-   },
-   "my-thoughts-on-learning-react": {
-      comments: []
-   }
-}
+const port = process.env.PORT || 8000;
 
 // Initializing a Middleware
 //    Function of express: It parses incoming JSON payload
-app.use(express.json({extended: false}));
+app.use(express.json({ extended: false }));
+
+// A function 'withDB()' to perform DB Operations (28.10.)
+const withDB = async (operations, res) => {
+   try {
+      // Connecting to the MongoDB Server through Express
+      const client = await MongoClient.connect('mongodb://127.0.0.1:27017');
+
+      // Fetching the DB
+      const db = client.db('mernblog');
+
+      // Performing Operations through a function 'operations()'
+      await operations(db);
+
+      // Closing the Connection
+      client.close();
+
+   } catch (err) {
+      // Internal Server Error
+      res.status(500).json({
+         status: 'fail',
+         message: 'Error connecting to the database!',
+         err
+      })
+   }
+};
+
+// Implementing Route for Articles (28.10.23)
+app.get('/api/articles/:name', async (req, res) => {
+
+   withDB(async (db) => {
+      const articleName = req.params.name;
+
+      // Fetching the 'articleInfo' from the DB
+      const articleInfo = await db
+         .collection('articles')
+         .findOne({ name: articleName });
+
+      // Sending back the response to the Client
+      res.status(200).json(articleInfo);
+   }, res)
+
+})
 
 // Routing the Comments
 app.post('/api/articles/:name/add-comments', (req, res) => {
-   const {username, text} = req.body;
+   const { username, text } = req.body;
    const articleName = req.params.name;
-   articlesInfo[articleName].comments.push({username, text});
-   // articlesInfo[articleName].comments.push(req.body);
 
-   res.status(200).send(articlesInfo[articleName]);
+   withDB(async (db) => {
+      // Fetching the Article Info
+      const articleInfo = await db
+         .collection('articles')
+         .findOne({ name: articleName });
+
+      // Updating the Article Info
+      await db.collection('articles')
+         .updateOne({ name: articleName },
+            {
+               $set: {
+                  comments: articleInfo.comments.concat({ username, text })
+               }
+            }
+         );
+
+      // Getting Updated Article Info
+      const updatedArticleInfo =  await db.collection('articles').findOne({name: articleName});
+
+      // Sending back Response to the Client
+      res.status(200).json(updatedArticleInfo);
+   }, res)
+});
+
+// For Testing Purpose
+app.get('/', (req, res) => {
+   res.status(200).json({
+      status: 'success',
+      message: 'Response from the server!'
+   })
 })
 
 // Starting the server
